3ac59b3e0ef1c4e4e8c46d261a7c0f1aeede12a2
ozziee <ozzieee@gmail.com>
2016-06-02 00:41:39 +0200

    Some clarifications

--- Programming_AVR/Laboratory_Assignments/Lab5/Lab_5_2/main.cpp
+++ Programming_AVR/Laboratory_Assignments/Lab5/Lab_5_2/main.cpp
@@ -9,9 +9,9 @@
 Clock clock;
 Counter tick(0, 30);	// 30 Overflows ~= 1 second
 
-ISR(TIMER0_OVF_vect)
+ISR(TIMER0_OVF_vect)	// Interrupts when Timer has encountered overflow
 {
-	if(tick.Increment())
+	if(tick.Increment())	// Returns 1 if tick has reached 30
 	{
 		clock.UpdateClock();
 	}
@@ -23,8 +23,8 @@ int main(void)
 
 	TCCR0|=(1<<CS02)|(1<<CS00);
 	// 8000000/1024 = 7812.5 Hz
-	// Counting to 255 will take 7812.5 / 255 = 30.6... s
-	// One second will take ~30 overflows
+	// Counting to 255 will take 7812.5 / 255 = 30.6...
+	// One second takes ~30 overflows
 
 	TIMSK|=(1<<TOIE0);	//Enable Overflow Interrupt
 	TCNT0=0;			//Initialize Counter
